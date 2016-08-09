178291d4a76acdf55c65106133230664dc49464a
rahuls5 <rahuls5@CHETUIWK1065>
2016-08-09 15:26:53 +0530

    Add project files.

---
+++ VPZAngularDemo1/Script/ng-src-directive.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="angular.js" />
+var myApp = angular
+                  .module("myModule", [])
+                  .controller("myController", function ($scope) {
+                      var country = {
+                          name: "India",
+                          capital: "New Delhi",
+                          flag: "/Image/JaiHo.jpg"
+                      };
+
+                      $scope.country = country;
+                  });
